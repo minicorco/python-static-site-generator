7341debd53f0f421f3036f8d86a14cedeb889cc8
minicorco <minicorco@gmail.com>
2022-11-17 10:54:55 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,18 +6,21 @@ class Parser:
     extensions:List[str]=[]
     def valid_extension(self,extension):
         return extension in self.extensions
-    def read(self,path):
-        with open(path,"r") as file:
-            return file.read()
-    def write(self,path,dest,content,ext:".html"):
-        full_path= dest / path.with_suffix(ext).name
-        with open(full_path,"w") as file:
-            file.write(content)
-    def copy(self,path,source,dest):
-        shutil.copy2(path,dest/path.relative_to(source))
     def parse(self,path:Path,source:Path,dest:Path):
         #els dos punts pareix que assignen tipus
         raise NotImplementedError
+        
+    def read(self,path):
+        with open(path,"r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+
+    def copy(self,path,source,dest):
+        shutil.copy2(path,dest/path.relative_to(source))
 
 class ResourceParser(Parser):
     extensions=[".jpg",".png",".gif",".css",".html"]
