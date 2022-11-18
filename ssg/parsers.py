db472f4e5b64ad7cc22c7d2749cd48c597d9891d
minicorco <minicorco@gmail.com>
2022-11-18 10:08:50 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,7 +46,7 @@ class ReStructuredTextParser(Parser):
 
     def parse(self,path,source,dest):
         content=Content.load(self.read(path))
-        html=publish_parts(content.body,writer_name:"html5")
+        html=publish_parts(content.body)
+        writer_name:"html15"
         self.write(path,dest,html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
-        
